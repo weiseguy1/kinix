60bfd8a9a81b16424afdeba7022d13a82f1e0d44
WeiseGuy <info@weiseguy.net>
2024-11-16 09:33:21 -0600

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+   home-manager = {
+     url = "github:nix-community/home-manager/release-24.05";
+     inputs.nixpkgs.follows = "nixpkgs";
+   };
+   nixvim = {
+     url = "github:nix-community/nixvim/nixos-24.05";
+     inputs.nixpkgs.follows = "nixpkgs";
+   };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      legacyPackages = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system:
+        import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+      );
+
+    in
+  {
+    nixosConfigurations = { 
+      framework13 = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      wg = home-manager.lib.homeManagerConfiguration {
+        pkgs = legacyPackages.x86_64-linux;
+        modules = [ 
+          ./home.nix
+          inputs.nixvim.homeManagerModules.nixvim 
+        ];
+      };
+    };
+  };
+}
