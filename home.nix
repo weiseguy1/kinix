f10ad1d4c89c232fd5b4cada7d6f5e656457ec84
WeiseGuy <info@weiseguy.net>
2024-11-16 09:55:19 -0600

    Initial setup of home-manager

---
+++ home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+
+  home = {
+    username = "wg";
+    homeDirectory = "/home/wg";
+    sessionPath = [ "$HOME/.local/bin" ];
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.05";
+    packages = with pkgs; [
+      tree
+      yt-dlp
+      brave
+      mpv
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    ];
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
