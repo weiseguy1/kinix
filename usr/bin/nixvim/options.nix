a907e3a9c737157a372f86c093b26308f38613d5
WeiseGuy <info@weiseguy.net>
2024-11-19 06:13:39 -0600

    Replacing tabs with spaces

--- usr/bin/nixvim/options.nix
+++ usr/bin/nixvim/options.nix
@@ -3,11 +3,12 @@
 {
   programs.nixvim.opts = {
     number = true;
-		relativenumber = true;
-		shiftwidth = 2;
-		tabstop = 2;
-		guifont = "monospace:h17";
-		scrolloff = 8;
-		sidescrolloff = 8;
-	};
+    relativenumber = true;
+    shiftwidth = 2;
+    tabstop = 2;
+    expandtab = true;
+    guifont = "monospace:h17";
+    scrolloff = 8;
+    sidescrolloff = 8;
+  };
 }
