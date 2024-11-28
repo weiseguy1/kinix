f47eda04ab98c8936177df87c4412590f45990aa
WeiseGuy <info@weiseguy.net>
2024-11-27 18:11:33 -0600

    Set alias for nix-shell

--- home.nix
+++ home.nix
@@ -12,6 +12,9 @@
     sessionVariables = {
       EDITOR = "nvim";
     };
+    shellAliases = {
+      shell = "nix-shell --command zsh";
+    };
 
     # You should not change this value, even if you update Home Manager. If you do
     # want to update the value, then make sure to first check the Home Manager
