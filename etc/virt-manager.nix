b7e521485590a3b0bbaf7cbfd4e5222f23b183ba
WeiseGuy <info@weiseguy.net>
2024-12-05 06:00:49 -0600

    Added virt-manager/fixed weird bug

--- etc/virt-manager.nix
+++ etc/virt-manager.nix
@@ -1,10 +1,29 @@
+{ pkgs, ... }:
+
 {
-  virtualisation.libvirtd = { 
-    enable = true;
-    qemu.ovmf.enable = true;
-    qemu.runAsRoot = false;
-    onBoot = "ignore";
-    onShutdown = "shutdown";
-  };
+  programs.dconf.enable = true;
   programs.virt-manager.enable = true;
+  environment.systemPackages = with pkgs; [
+    # https://github.com/NixOS/nixpkgs/issues/267579
+    (virt-manager.overrideAttrs (old: {
+      nativeBuildInputs = old.nativeBuildInputs ++ [wrapGAppsHook];
+      buildInputs = lib.lists.subtractLists [wrapGAppsHook] old.buildInputs ++ [
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+      ];
+    }))
+
+    spice spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+  ];
+
+  virtualisation = {
+    libvirtd = { 
+      enable = true;
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
 }
