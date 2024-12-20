3ea97d4ea801c5d7353ed56278b9d8600058d365
WeiseGuy <info@weiseguy.net>
2024-12-20 08:09:14 -0600

    Created new home-manager user

---
+++ home/kweise.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./usr/bin
+  ];
+
+  home = {
+    username = "kweise";
+    homeDirectory = "/home/kweise`";
+    sessionPath = [ "$HOME/.local/bin" ];
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+    shellAliases = {
+      shell = "nix-shell --command zsh";
+    };
+
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.05";
+    packages = with pkgs; [
+      spotify
+      obsidian
+      tree
+      yt-dlp
+      brave
+      mpv
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    ];
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
