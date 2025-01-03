aec9db3b378fbea951db0dc4bdd9dfe0beaed20d
WeiseGuy <info@weiseguy.net>
2025-01-02 21:53:27 -0600

    Messing with inputs flake.nix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-   home-manager = {
-     url = "github:nix-community/home-manager/release-24.11";
-     inputs.nixpkgs.follows = "nixpkgs";
-   };
-   nixvim = {
-     url = "github:nix-community/nixvim/nixos-24.11";
-     inputs.nixpkgs.follows = "nixpkgs";
-   };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
