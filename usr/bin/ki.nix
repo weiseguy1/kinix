50fdd26dd236ce97365d967f5f28ca921e2c7866
WeiseGuy <info@weiseguy.net>
2025-01-07 20:41:50 -0600

    Removed usr/bin dir

--- usr/bin/ki.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-
-pkgs.writeShellScriptBin "kinix" ''
-    if [[ `git -C $HOME/.config/nixos status --porcelain` ]]; then
-        echo "WARNING: repo has unstaged files!"
-    fi
-    #sudo nixos-rebuild switch --flake ~/.config/nixos#default
-''
